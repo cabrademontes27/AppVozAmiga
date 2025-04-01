f98383d0c13080f01515d41c703bd5e2ef3be7ff
cabrademontes27 <cabrademontes@gmail.com>
2025-03-31 18:35:39 -0600

    new imports

--- app/src/main/java/com/example/appvozamiga/MainActivity.kt
+++ app/src/main/java/com/example/appvozamiga/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.appvozamiga.ui.login.Ui.RegisterScreen
+import com.example.appvozamiga.ui.login.Ui.RegisterViewModel
 import com.example.appvozamiga.ui.theme.AppVozAmigaTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +20,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RegisterScreen()
+            RegisterScreen(RegisterViewModel())
         }
     }
 }
