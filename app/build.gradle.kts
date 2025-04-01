0e4a68fc66070a24f4f950272a429e4b973cd303
cabrademontes27 <cabrademontes@gmail.com>
2025-03-31 18:34:34 -0600

    viewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,12 @@ android {
 
 dependencies {
 
+
+    implementation (libs.lifecycle.viewmodel.ktx)
+    implementation (libs.androidx.lifecycle.livedata.ktx)
+    implementation (libs.androidx.runtime.livedata)
+    implementation (libs.androidx.lifecycle.livedata.ktx)
+    implementation (libs.androidx.runtime.livedata)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
