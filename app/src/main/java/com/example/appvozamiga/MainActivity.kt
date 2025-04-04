5b8d822b52bf2f9477b52a3e67bfde3839326888
cabrademontes27 <cabrademontes@gmail.com>
2025-04-04 08:50:45 -0600

    delay

--- app/src/main/java/com/example/appvozamiga/MainActivity.kt
+++ app/src/main/java/com/example/appvozamiga/MainActivity.kt
@@ -4,24 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.appvozamiga.ui.login.Ui.RegisterScreen
 import com.example.appvozamiga.ui.login.Ui.RegisterViewModel
+import com.example.appvozamiga.ui.login.Ui.SplashScreen
 import com.example.appvozamiga.ui.theme.AppVozAmigaTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RegisterScreen(RegisterViewModel())
+            val showSplash = remember { mutableStateOf(true) }
+
+            if (showSplash.value) {
+                SplashScreen()
+                LaunchedEffect(Unit) {
+                    delay(2000) // 2 seconds delay
+                    showSplash.value = false
+                }
+            } else {
+                val viewModel: RegisterViewModel by viewModels()
+                RegisterScreen(viewModel)
+            }
         }
     }
 }
+
 
---
+++ app/src/main/java/com/example/appvozamiga/ui/login/Ui/SplashScreen.kt
@@ -0,0 +1,4 @@
+package com.example.appvozamiga.ui.login.Ui
+
+class SplashScreen {
+}
