b37c37059ce7b13d1e75b932f261ea06f457061f
cabrademontes <cabrademontes@gmail.com>
2025-04-05 13:58:44 -0600

    Nav in the app

--- app/src/main/java/com/example/appvozamiga/ui/Navigation/Navigation.kt
+++ app/src/main/java/com/example/appvozamiga/ui/Navigation/Navigation.kt
@@ -1,2 +1,50 @@
 package com.example.appvozamiga.ui.Navigation
 
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.appvozamiga.ui.Screen.register.RegisterScreen
+import com.example.appvozamiga.ui.Screen.register.VerificationScreen
+import com.example.appvozamiga.ui.Screen.splash.SplashScreen
+
+@Composable
+fun AppNavigation() {
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Routes.SPLASH
+    ) {
+        composable(Routes.SPLASH) {
+            SplashScreen(
+                onTimeout = {
+                    navController.navigate(Routes.REGISTER) {
+                        // Esto evita que el usuario pueda volver atrás al Splash
+                        popUpTo(Routes.SPLASH) { inclusive = true }
+                    }
+                }
+            )
+        }
+
+        composable(Routes.REGISTER) {
+            RegisterScreen(viewModel = viewModel()) {
+                navController.navigate(Routes.VERIFICATION)
+            }
+        }
+
+        composable(Routes.VERIFICATION) {
+            VerificationScreen {
+                navController.navigate("mainMenu") {
+                    popUpTo(Routes.VERIFICATION) { inclusive = true }
+                }
+            }
+        }
+
+
+
+    }
+}
+
--- app/src/main/java/com/example/appvozamiga/ui/Navigation/Routes.kt
+++ app/src/main/java/com/example/appvozamiga/ui/Navigation/Routes.kt
@@ -1,2 +1,7 @@
 package com.example.appvozamiga.ui.Navigation
 
+object Routes {
+    const val SPLASH = "splash"
+    const val REGISTER = "register"
+    const val VERIFICATION = "verification"
+}
