a169b8f67d61aed932102eeaf89ac779d77fb7e0
cabrademontes <cabrademontes@gmail.com>
2025-04-06 17:27:13 -0600

    user repository

--- app/src/main/java/com/example/appvozamiga/repository/mongodb/MongoRepository.kt
+++ app/src/main/java/com/example/appvozamiga/repository/mongodb/MongoRepository.kt
@@ -1,2 +1,18 @@
-package com.example.appvozamiga.backend.mongodb
+package com.example.appvozamiga.repository.mongodb
 
+import android.util.Log
+import com.example.appvozamiga.repository.mongodb.models.UserData
+
+object MongoUserRepository {
+
+    //  Esta versión es la correcta para Retrofit + coroutines
+    suspend fun registerUser(user: UserData): Boolean {
+        return try {
+            val response = RetrofitClient.apiService.registerUser(user)
+            response.isSuccessful
+        } catch (e: Exception) {
+            Log.e("MongoRepo", "Error Retrofit: ${e.message}")
+            false
+        }
+    }
+}
