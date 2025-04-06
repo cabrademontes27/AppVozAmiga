35ba94c6dc586dccbd64b0d264e123562cc040ea
cabrademontes <cabrademontes@gmail.com>
2025-04-06 17:25:35 -0600

    conecction api

--- app/src/main/java/com/example/appvozamiga/repository/mongodb/MongoApiService.kt
+++ app/src/main/java/com/example/appvozamiga/repository/mongodb/MongoApiService.kt
@@ -1,2 +1,12 @@
-package com.example.appvozamiga.repository.mongodb 
+package com.example.appvozamiga.repository.mongodb
 
+import com.example.appvozamiga.repository.mongodb.models.UserData
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface MongoApiService {
+
+    @POST("api/registerUser") 
+    suspend fun registerUser(@Body user: UserData): Response<Unit>
+}
