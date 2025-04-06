270b90e1693c0d48d3542288f7acd65c064bf716
cabrademontes <cabrademontes@gmail.com>
2025-04-06 17:28:37 -0600

    new flow

--- app/src/main/java/com/example/appvozamiga/MainActivity.kt
+++ app/src/main/java/com/example/appvozamiga/MainActivity.kt
@@ -5,11 +5,37 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.appvozamiga.ui.Navigation.AppNavigation
+import com.google.firebase.auth.FirebaseAuth
+import android.util.Log
+import androidx.activity.viewModels
+import com.example.appvozamiga.ViewModels.RegisterViewModel
+
 
 class MainActivity : ComponentActivity() {
+    private val registerViewModel: RegisterViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        val emailLink = intent?.data?.toString()
+        val prefs = getSharedPreferences("user_prefs", MODE_PRIVATE)
+        val savedEmail = prefs.getString("email_for_signin", null)
+
+        if (emailLink != null && FirebaseAuth.getInstance().isSignInWithEmailLink(emailLink) && savedEmail != null) {
+            FirebaseAuth.getInstance()
+                .signInWithEmailLink(savedEmail, emailLink)
+                .addOnSuccessListener {
+                    Log.d("MainActivity", "✅ Usuario autenticado correctamente")
+                    prefs.edit().putBoolean("is_registered", true).apply()
+
+                    // ⬅️ Aquí notificamos al ViewModel
+                    registerViewModel.setUserVerified()
+                }
+                .addOnFailureListener {
+                    Log.e("MainActivity", "❌ Error de autenticación", it)
+                }
+        }
+
         setContent {
             AppNavigation()
         }
