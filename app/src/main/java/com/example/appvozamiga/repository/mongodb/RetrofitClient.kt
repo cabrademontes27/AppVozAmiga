f8eacf724f9f659a2bf0223882d9bcf9ee4ae218
cabrademontes <cabrademontes@gmail.com>
2025-04-06 17:31:58 -0600

    connection api

--- app/src/main/java/com/example/appvozamiga/repository/mongodb/RetrofitClient.kt
+++ app/src/main/java/com/example/appvozamiga/repository/mongodb/RetrofitClient.kt
@@ -1,2 +1,17 @@
 package com.example.appvozamiga.repository.mongodb
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+
+    private const val BASE_URL = "http://192.168.0.10:3000/"
+
+    val apiService: MongoApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(MongoApiService::class.java)
+    }
+}
