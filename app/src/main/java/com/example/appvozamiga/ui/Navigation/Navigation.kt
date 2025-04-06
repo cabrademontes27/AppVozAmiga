21011044a3b7246f53842fa29b79156ac990cb78
cabrademontes <cabrademontes@gmail.com>
2025-04-06 17:23:44 -0600

    navigations screens

--- app/src/main/java/com/example/appvozamiga/ui/Navigation/Navigation.kt
+++ app/src/main/java/com/example/appvozamiga/ui/Navigation/Navigation.kt
@@ -1,17 +1,23 @@
 package com.example.appvozamiga.ui.Navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.appvozamiga.ViewModels.RegisterViewModel
+import com.example.appvozamiga.repository.fireBase.isUserRegistered
+import com.example.appvozamiga.ui.Screen.Menu.MainMenuScreen
 import com.example.appvozamiga.ui.Screen.register.RegisterScreen
-import com.example.appvozamiga.ui.Screen.register.VerificationScreen
+import com.example.appvozamiga.ui.Screen.splash.LoadingScreen
 import com.example.appvozamiga.ui.Screen.splash.SplashScreen
+import com.example.appvozamiga.ui.Screen.splash.SuccessScreen
+import kotlinx.coroutines.delay
 
 @Composable
 fun AppNavigation() {
-
     val navController = rememberNavController()
 
     NavHost(
@@ -19,32 +25,59 @@ fun AppNavigation() {
         startDestination = Routes.SPLASH
     ) {
         composable(Routes.SPLASH) {
-            SplashScreen(
-                onTimeout = {
+            val context = LocalContext.current
+            SplashScreen()
+
+            LaunchedEffect(Unit) {
+                delay(2000)
+                if (isUserRegistered(context)) {
+                    navController.navigate(Routes.MAIN_MENU) {
+                        popUpTo(Routes.SPLASH) { inclusive = true }
+                    }
+                } else {
                     navController.navigate(Routes.REGISTER) {
-                        // Esto evita que el usuario pueda volver atrás al Splash
                         popUpTo(Routes.SPLASH) { inclusive = true }
                     }
                 }
-            )
-        }
-
-        composable(Routes.REGISTER) {
-            RegisterScreen(viewModel = viewModel()) {
-                navController.navigate(Routes.VERIFICATION)
             }
         }
 
-        composable(Routes.VERIFICATION) {
-            VerificationScreen {
-                navController.navigate("mainMenu") {
-                    popUpTo(Routes.VERIFICATION) { inclusive = true }
+        composable(Routes.REGISTER) {
+            val registerViewModel: RegisterViewModel = viewModel()
+            val state = registerViewModel.uiState.value
+
+            when {
+                state.isLoading -> {
+                    LoadingScreen()
+                }
+                state.isSuccess -> {
+                    SuccessScreen(onFinish = {
+                        navController.navigate(Routes.MAIN_MENU) {
+                            popUpTo(Routes.REGISTER) { inclusive = true }
+                        }
+                    })
+                }
+                else -> {
+                    RegisterScreen(viewModel = registerViewModel) {
+                        navController.navigate(Routes.MAIN_MENU) {
+                            popUpTo(Routes.REGISTER) { inclusive = true }
+                        }
+                    }
                 }
             }
         }
 
+        composable(Routes.SUCCESS) {
+            SuccessScreen(onFinish = {
+                navController.navigate(Routes.MAIN_MENU) {
+                    popUpTo(Routes.SUCCESS) { inclusive = true }
+                }
+            })
+        }
 
-
+        composable(Routes.MAIN_MENU) {
+            // Esta sería la pantalla principal de la app
+            MainMenuScreen()
+        }
     }
 }
-
--- app/src/main/java/com/example/appvozamiga/ui/Navigation/Routes.kt
+++ app/src/main/java/com/example/appvozamiga/ui/Navigation/Routes.kt
@@ -3,5 +3,6 @@ package com.example.appvozamiga.ui.Navigation
 object Routes {
     const val SPLASH = "splash"
     const val REGISTER = "register"
-    const val VERIFICATION = "verification"
+    const val MAIN_MENU = "mainMenu"
+    const val SUCCESS = "success"
 }
