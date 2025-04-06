ba244bf94118c60ee106cf5d7a114b3ac9322e3e
cabrademontes <cabrademontes@gmail.com>
2025-04-06 17:25:14 -0600

    model User

--- app/src/main/java/com/example/appvozamiga/repository/mongodb/models/UserData.kt
+++ app/src/main/java/com/example/appvozamiga/repository/mongodb/models/UserData.kt
@@ -1,2 +1,18 @@
 package com.example.appvozamiga.repository.mongodb.models
 
+data class UserData(
+    val name: String,
+    val lastName: String,
+    val secondLastName: String,
+    val email: String,
+    val telephone: String,
+    val birthDay: String,
+    val location: Location
+)
+
+data class  Location(
+    val state: String,
+    val municipality: String,
+    val colony: String,
+    val street: String
+)
