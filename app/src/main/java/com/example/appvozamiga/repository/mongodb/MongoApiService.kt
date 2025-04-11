6019b41db35b493c65484d95e0d8ef19b0ee21c5
cabrademontes <cabrademontes@gmail.com>
2025-04-11 13:28:57 -0600

    fun check email and token

--- app/src/main/java/com/example/appvozamiga/repository/mongodb/MongoApiService.kt
+++ app/src/main/java/com/example/appvozamiga/repository/mongodb/MongoApiService.kt
@@ -1,12 +1,23 @@
 package com.example.appvozamiga.repository.mongodb
 
 import com.example.appvozamiga.repository.mongodb.models.UserData
+import com.example.appvozamiga.repository.mongodb.models.VerificationStatus
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface MongoApiService {
 
     @POST("api/registerUser")
     suspend fun registerUser(@Body user: UserData): Response<Unit>
+
+    @GET("api/checkVerified")
+    suspend fun checkEmailVerified(@Query("email") email: String): Response<VerificationStatus>
+
+    @GET("verify")
+    suspend fun verifyToken(@Query("id") tokenId: String): Response<String>
+
+
 }
--- app/src/main/java/com/example/appvozamiga/repository/mongodb/MongoRepository.kt
+++ app/src/main/java/com/example/appvozamiga/repository/mongodb/MongoRepository.kt
@@ -2,6 +2,7 @@ package com.example.appvozamiga.repository.mongodb
 
 import android.util.Log
 import com.example.appvozamiga.repository.mongodb.models.UserData
+import com.example.appvozamiga.repository.mongodb.models.VerificationStatus
 
 object MongoUserRepository {
 
@@ -13,6 +14,28 @@ object MongoUserRepository {
         } catch (e: Exception) {
             Log.e("MongoRepo", "Error Retrofit: ${e.message}")
             false
+        }
+    }
+    suspend fun checkEmailVerified(email: String): VerificationStatus? {
+
+        return try {
+            val response = RetrofitClient.apiService.checkEmailVerified(email)
+            Log.d("RegisterViewModel", "Resultado: ${response.body()?.verified}")
+            if (response.isSuccessful) response.body() else null
+        } catch (e: Exception) {
+            Log.e("MongoRepo", "Error checkEmailVerified: ${e.message}")
+            null
+        }
+    }
+
+
+    suspend fun verifyToken(tokenId: String): String? {
+        return try {
+            val response = RetrofitClient.apiService.verifyToken(tokenId)
+            if (response.isSuccessful) response.body() else null
+        } catch (e: Exception) {
+            Log.e("MongoRepo", "Error verifyToken: ${e.message}")
+            null
         }
     }
 }
