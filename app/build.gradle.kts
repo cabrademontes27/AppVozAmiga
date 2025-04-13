6957b8c90b7dd31d847cc274c4d0b8e9afebbfce
cabrademontes <cabrademontes@gmail.com>
2025-04-12 18:30:11 -0600

    new dependencies to use the location and camera

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,13 @@ android {
 
 dependencies {
 
+    implementation ("com.google.android.gms:play-services-location:21.0.1")
+    implementation ("androidx.activity:activity-compose:1.8.0")
+    implementation ("com.google.mlkit:text-recognition:16.0.1")
+    implementation ("androidx.camera:camera-camera2:1.3.1")
+    implementation ("androidx.camera:camera-lifecycle:1.3.1")
+    implementation ("androidx.camera:camera-view:1.3.1")
+    implementation ("com.google.mlkit:text-recognition:16.0.1")
     implementation(libs.kotlinx.coroutines.android)
     implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation ("com.squareup.retrofit2:retrofit:2.11.0")
--- app/src/main/java/com/example/appvozamiga/utils/Extensions.kt
+++ app/src/main/java/com/example/appvozamiga/utils/Extensions.kt
@@ -1,2 +1,30 @@
 package com.example.appvozamiga.utils
 
+import android.graphics.Bitmap
+import android.graphics.ImageFormat
+import android.graphics.YuvImage
+import androidx.camera.core.ImageProxy
+import java.io.ByteArrayOutputStream
+import java.nio.ByteBuffer
+
+fun ImageProxy.toBitmap(): Bitmap? {
+    val yBuffer = planes[0].buffer
+    val uBuffer = planes[1].buffer
+    val vBuffer = planes[2].buffer
+
+    val ySize = yBuffer.remaining()
+    val uSize = uBuffer.remaining()
+    val vSize = vBuffer.remaining()
+
+    val nv21 = ByteArray(ySize + uSize + vSize)
+
+    yBuffer.get(nv21, 0, ySize)
+    vBuffer.get(nv21, ySize, vSize)
+    uBuffer.get(nv21, ySize + vSize, uSize)
+
+    val yuvImage = YuvImage(nv21, ImageFormat.NV21, width, height, null)
+    val out = ByteArrayOutputStream()
+    yuvImage.compressToJpeg(android.graphics.Rect(0, 0, width, height), 100, out)
+    val imageBytes = out.toByteArray()
+    return android.graphics.BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+}
