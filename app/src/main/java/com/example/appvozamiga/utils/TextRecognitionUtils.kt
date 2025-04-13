c11c322a3596f74638a1fdf57a4d87d5edffad5c
cabrademontes <cabrademontes@gmail.com>
2025-04-12 18:32:32 -0600

    fun to recognize text from bitmap

--- app/src/main/java/com/example/appvozamiga/utils/TextRecognitionUtils.kt
+++ app/src/main/java/com/example/appvozamiga/utils/TextRecognitionUtils.kt
@@ -1,2 +1,23 @@
 package com.example.appvozamiga.utils
 
+
+
+import android.graphics.Bitmap
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+
+object TextRecognitionUtils {
+    fun recognizeTextFromBitmap(
+        bitmap: Bitmap,
+        onSuccess: (String) -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
+        val image = InputImage.fromBitmap(bitmap, 0)
+        val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+        recognizer.process(image)
+            .addOnSuccessListener { result -> onSuccess(result.text) }
+            .addOnFailureListener { e -> onFailure(e) }
+    }
+}
