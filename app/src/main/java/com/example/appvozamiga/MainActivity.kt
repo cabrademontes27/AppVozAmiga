f0ee81f6067d3f337828b671a78e2eb1a23819f6
cabrademontes <cabrademontes@gmail.com>
2025-04-12 18:27:01 -0600

    Updated imports after restructuring files

--- app/src/main/java/com/example/appvozamiga/MainActivity.kt
+++ app/src/main/java/com/example/appvozamiga/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import com.example.appvozamiga.ui.Navigation.AppNavigation
-import com.example.appvozamiga.ViewModels.RegisterViewModel
+import com.example.appvozamiga.ui.navigation.AppNavigation
+import com.example.appvozamiga.viewModels.register.RegisterViewModel
 
 class MainActivity : ComponentActivity() {
     private val registerViewModel: RegisterViewModel by viewModels()
