b5edeb14da8ce80de45c7607c59b3654f68c425a
cabrademontes <cabrademontes@gmail.com>
2025-05-01 11:09:06 -0600

    refactor types of variables to not null

--- app/src/main/java/com/example/appvozamiga/data/models/DrugsData.kt
+++ app/src/main/java/com/example/appvozamiga/data/models/DrugsData.kt
@@ -1,8 +1,8 @@
 package com.example.appvozamiga.data.models
 
 data class Medications(
-    val name: String,
-    val description: String,
-    val emailUser: String,
+    val name: String = "",
+    val description: String = "",
+    val emailUser: String = "",
     val _id: String? = null
 )
