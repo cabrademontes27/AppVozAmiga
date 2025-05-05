7cb875d42bd74dd5caef228ff2a84bde1cdf5e2c
cabrademontes <cabrademontes@gmail.com>
2025-05-04 22:22:27 -0600

    added new class LoginUiState

--- app/src/main/java/com/example/appvozamiga/viewModels/login/LoginUiState.kt
+++ app/src/main/java/com/example/appvozamiga/viewModels/login/LoginUiState.kt
@@ -1,2 +1,10 @@
 package com.example.appvozamiga.viewModels.login
 
+import com.example.appvozamiga.data.models.UserData
+
+sealed class LoginUiState {
+    object Idle : LoginUiState()
+    object Loading : LoginUiState()
+    data class Success(val user: UserData) : LoginUiState()
+    data class Error(val message: String) : LoginUiState()
+}
