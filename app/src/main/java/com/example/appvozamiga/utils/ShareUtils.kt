404e1cbcf79ddd06ccfca436b0d4f1e3fd3bafb6
cabrademontes <cabrademontes@gmail.com>
2025-05-06 18:04:46 -0600

    save qr in temporal directory and share with other apps

--- app/src/main/java/com/example/appvozamiga/utils/ShareUtils.kt
+++ app/src/main/java/com/example/appvozamiga/utils/ShareUtils.kt
@@ -1,2 +1,33 @@
 package com.example.appvozamiga.utils
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Environment
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.FileOutputStream
+
+fun shareBitmap(context: Context, bitmap: Bitmap) {
+    val cachePath = File(context.cacheDir, "images")
+    cachePath.mkdirs()
+    val file = File(cachePath, "qr.png")
+    FileOutputStream(file).use { out ->
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
+    }
+
+    val contentUri: Uri = FileProvider.getUriForFile(
+        context,
+        "${context.packageName}.provider", // asegúrate de que coincida con tu AndroidManifest
+        file
+    )
+
+    val shareIntent = Intent(Intent.ACTION_SEND).apply {
+        type = "image/png"
+        putExtra(Intent.EXTRA_STREAM, contentUri)
+        addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+    }
+
+    context.startActivity(Intent.createChooser(shareIntent, "Compartir QR"))
+}
