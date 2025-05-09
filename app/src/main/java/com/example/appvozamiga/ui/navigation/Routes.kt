69f5ac9e549a5c0759c2d0f09bb4529b14cad207
cabrademontes <cabrademontes@gmail.com>
2025-05-09 10:03:59 -0600

    remove loading screens

--- app/src/main/java/com/example/appvozamiga/ui/navigation/Routes.kt
+++ app/src/main/java/com/example/appvozamiga/ui/navigation/Routes.kt
@@ -18,9 +18,5 @@ object Routes {
     const val CAMERA = "camera"
     const val ABOUT_ME = "about_me"
 
-    // Loading transitions
-    const val LOADING_TO_DRUGS = "loading_to_drugs"
-    const val LOADING_TO_LOCATION = "loading_to_location"
-    const val LOADING_TO_CAMERA = "loading_to_camera"
-    const val LOADING_TO_ABOUT_ME = "loading_to_about_me"
+
 }
