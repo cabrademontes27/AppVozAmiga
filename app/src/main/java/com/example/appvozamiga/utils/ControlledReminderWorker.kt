300d4b3f7a194e244500eb04b1012f4c4b707685
cabrademontes <cabrademontes@gmail.com>
2025-05-17 05:01:30 -0600

    notifications

--- app/src/main/java/com/example/appvozamiga/utils/ControlledReminderWorker.kt
+++ app/src/main/java/com/example/appvozamiga/utils/ControlledReminderWorker.kt
@@ -1,2 +1,45 @@
 package com.example.appvozamiga.utils
 
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.example.appvozamiga.R
+
+class ControlledReminderWorker(appContext: Context, workerParams: WorkerParameters)
+    : Worker(appContext, workerParams) {
+
+    override fun doWork(): Result {
+        val name = inputData.getString("name") ?: return Result.failure()
+
+        showNotification(name)
+        return Result.success()
+    }
+
+    private fun showNotification(name: String) {
+        val notificationManager = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channelId = "meds_controlled"
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Medicamentos controlados",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = NotificationCompat.Builder(applicationContext, channelId)
+            .setContentTitle("⏰ Recordatorio de medicamento")
+            .setContentText("Pronto te toca tomar: $name")
+            .setSmallIcon(R.drawable.icon_medicament)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .build()
+
+        notificationManager.notify(name.hashCode(), notification)
+    }
+}
