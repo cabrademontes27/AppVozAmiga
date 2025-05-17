13b458d231f9c4ac1a79803e03c3b5795b36011e
cabrademontes <cabrademontes@gmail.com>
2025-05-17 04:58:09 -0600

    flow  controlled_medications

--- app/src/main/java/com/example/appvozamiga/ui/navigation/Navigation.kt
+++ app/src/main/java/com/example/appvozamiga/ui/navigation/Navigation.kt
@@ -41,6 +41,7 @@ import com.example.appvozamiga.data.models.saveSignInEmail
 import com.example.appvozamiga.data.models.setUserRegistered
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.mutableStateListOf
+import com.example.appvozamiga.ui.screen.menu.functions.ControlledDrugsScreen
 import com.example.appvozamiga.ui.screen.menu.functions.LinkingScreen
 import com.example.appvozamiga.ui.screen.menu.functions.QrScreen
 import com.example.appvozamiga.ui.screen.menu.functions.SOSScreen
@@ -216,6 +217,11 @@ fun AppNavigation() {
             composable(Routes.LINKING) {
                 LinkingScreen(viewModel = mainViewModel)
             }
+
+            composable(Routes.CONTROLLED_DRUGS) {
+                ControlledDrugsScreen(navController)
+            }
+
 
 
         }
